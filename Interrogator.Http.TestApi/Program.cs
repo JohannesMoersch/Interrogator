6fcf7261433918a2ba13f1250d34edddbd828c09
Johannes Moersch <johannes.moersch@gmail.com>
2022-04-01 20:40:02 -0600

    Updates test projects to .net 6.0

--- Interrogator.Http.TestApi/Program.cs
+++ Interrogator.Http.TestApi/Program.cs
@@ -1,24 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore;
 
 namespace Interrogator.Http.TestApi
 {
 	public class Program
 	{
 		public static void Main(string[] args)
-		{
-			CreateWebHostBuilder(args).Build().Run();
-		}
+			=> CreateWebHostBuilder(args)
+				.Build()
+				.Run();
 
-		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-			WebHost.CreateDefaultBuilder(args)
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+			=> WebHost
+				.CreateDefaultBuilder(args)
 				.UseStartup<Startup>();
 	}
 }
--- Interrogator.Http.TestApi/Startup.cs
+++ Interrogator.Http.TestApi/Startup.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Interrogator.Http.TestApi
 {
@@ -22,29 +11,19 @@ namespace Interrogator.Http.TestApi
 
 		public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+			services.AddControllers();
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
-			app.Use(d => c => d.Invoke(c));
+			app.UseRouting();
 
-			if (env.IsDevelopment())
+			app.UseEndpoints(endpoints =>
 			{
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				//app.UseHsts();
-			}
-
-			// app.UseHttpsRedirection();
-			app.UseMvc();
+				endpoints.MapControllers();
+			});
 		}
 	}
 }
